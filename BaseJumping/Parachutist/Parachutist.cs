d1e9e291f71cecbd786bef533a5ff75c16b8745a
Ainur Galimzyanov <a.galimz@mail.ru>
2022-04-28 03:44:46 +0500

    Добавьте файлы проекта.

---
+++ BaseJumping/Parachutist/Parachutist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseJumping.Parachutist
+{
+    class Parachutist
+    {
+        public int posX;
+        public int posY;
+
+        public int dirX;
+        public int dirY;
+        public bool isMoving;
+
+        public int flip;
+
+        public int landingFrames;
+        public int flightFrames;
+
+        public int size;
+
+        public Image spriteSheet;
+        
+        public Parachutist(int posX, int posY, int idleFrames, int runFrames, int attackFrames, int deathFrames, Image spriteSheet)
+        {
+
+        }
+    }
+}
